6b3d4ad9e5f0855e0e91c1a104d3ae9368ce0c94
parango33 <60206994+parango33@users.noreply.github.com>
2022-05-15 11:02:07 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from copyreg import pickle
+from email.policy import default
+import re
+from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+import pickle
+import numpy as np
+
+
+app= Flask(__name__)
+
+#Abrir modelo pre-entrenado
+model = pickle.load(open('naivebayes.pkl','rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    id= request.form['identification_type']
+    site=request.form['site']
+    gender=request.form['gender']
+    age = request.form['age']
+    marital = request.form['marital_status']
+    children = request.form['number_of_children']
+    blood = request.form['blood_type']
+    rh = request.form['rh_blood']
+    department = request.form['department']
+    city = request.form['city']
+    stratum = request.form['stratum']
+    dependent = request.form['depend_people']
+    hear = request.form['hear_about_us']
+    aq = request.form['have_acquaintances']
+    family = request.form['have_family_contact_center']
+    referal = request.form['referral_external']
+    transportation = request.form['have_transportation']
+    ed = request.form['education_level']
+    eng = request.form['learn_english']
+    worked = request.form['worked_before_call_center']
+    left = request.form['has_left']
+    returns = request.form['number_of_returns']
+
+    #arreglo con datos
+    employee = np.array([[id,site,gender,age,marital,children,blood,rh,department,
+                        city,stratum, dependent, hear, aq, family,referal,transportation,
+                        ed, eng, worked, left, returns]])
+
+    #predecir
+    pred = model.predict(employee)
+
+    #retorna
+    return render_template('after.html',data=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)    
---
+++ naivebayes.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB, BernoulliNB, CategoricalNB, ComplementNB, MultinomialNB
+from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score, f1_score, accuracy_score
+
+import pickle
+
+import ast
+
+#Importar datos
+df_test = pd.read_csv('datos_ready.csv')
+
+#Fijar el Target para el modelo
+target = df_test.non_risk_employee
+inputs = df_test.drop('non_risk_employee', axis='columns')
+
+#Separar datos train y test
+x_train, x_test, y_train, y_test = train_test_split(inputs,target,test_size=0.1)
+
+#modelo
+model = GaussianNB()
+model.fit(x_train,y_train)
+model.score(x_test,y_test)
+
+#Exportar modelo
+pickle.dump(model,open('naivebayes.pkl','wb'))
+
